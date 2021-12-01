dc060178221a223938c73f1b4c96ea3324b0fc3a
acrab <acrab@live.co.uk>
2021-12-01 22:15:46 +0000

    Day 01, part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -15,14 +15,23 @@ fun main() {
     fun part1(input: List<String>): Int = input.map { it.toInt() }.countIncreases()
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val ints = input.map { it.toInt() }
+        val count = ints.size
+        val firsts = ints.slice(0..count - 3)
+        val seconds = ints.slice(1..count - 2)
+        val thirds = ints.slice(2..count - 1)
+        val partialWindow = firsts.zip(seconds){a, b -> a+b}
+        val windows = partialWindow.zip(thirds){a,b -> a+b}
+
+        return windows.countIncreases()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
 }
