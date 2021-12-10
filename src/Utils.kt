dd70cd256e64c96a75b23bbb295425711622283b
acrab <acrab@live.co.uk>
2021-12-10 19:10:38 +0000

    Day09, parts one and two - Added some utility methods for dealing with 2D grids of things

--- src/Utils.kt
+++ src/Utils.kt
@@ -11,3 +11,23 @@ fun readInput(name: String) = File("src", "$name.txt").readLines()
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+fun checkResult(actual: Int, expected: Int) = check(actual == expected) { "Expected $expected, but got $actual" }
+
+fun <T> List<List<T>>.forEachGrid(action: (x: Int, y: Int, T) -> Unit) {
+    forEachIndexed { x, row -> row.forEachIndexed { y, it -> action(x, y, it) } }
+}
+
+fun <T> List<List<T>>.gridSum(action: (x: Int, y: Int, T) -> Int): Int {
+    var sum = 0
+    forEachIndexed { x, row -> row.forEachIndexed { y, it -> sum += action(x, y, it) } }
+    return sum
+}
+
+fun <T> List<List<T>>.neighbours(x: Int, y: Int, default: T): List<T> {
+    val up = if (x > 0) this[x - 1][y] else default
+    val down = if (x < this.size - 1) this[x + 1][y] else default
+    val left = if (y > 0) this[x][y - 1] else default
+    val right = if (y < this[x].size - 1) this[x][y + 1] else default
+    return listOf(up, right, down, left)
+}
---
+++ src/day09/day09.kt
@@ -0,0 +1,84 @@
+package day09
+
+import checkResult
+import forEachGrid
+import gridSum
+import neighbours
+import readInput
+
+class Group(private val members: MutableList<GridPoint> = mutableListOf()) {
+    fun mergeWith(other: Group) {
+        other.members.forEach {
+            it.group = this
+            members.add(it)
+        }
+    }
+
+    val size: Int
+        get() = members.size
+
+    fun add(gridPoint: GridPoint){
+        members.add(gridPoint)
+        gridPoint.group = this
+    }
+}
+
+data class GridPoint(val value: Int, var group: Group? = null)
+
+fun main() {
+
+    fun getInput(name: String) = readInput(name).map { line -> line.toCharArray().map { it.digitToInt() } }
+
+    fun calculateRisk(input: Int) = input + 1
+
+    fun part1(grid: List<List<Int>>): Int =
+        grid.gridSum { x, y, i ->
+            if (grid.neighbours(x, y, 10).all { i < it }) {
+                calculateRisk(i)
+            } else {
+                0
+            }
+        }
+
+    fun part2(grid: List<List<Int>>): Int {
+        val points = grid.map { row -> row.map { GridPoint(it) } }
+        val groups = mutableListOf<Group>()
+        points.forEachGrid { x, y, gridPoint ->
+            //Value 9s are the walls between groups
+            if(gridPoint.value == 9) return@forEachGrid
+
+            val neighbours = points.neighbours(x, y, null).filterNotNull()
+            val existingGroups = neighbours.mapNotNull { it.group }
+            if (existingGroups.isEmpty()) {
+                //make a new group
+                with(Group()) {
+                    add(gridPoint)
+                    groups.add(this)
+                }
+            } else if (existingGroups.size == 1) {
+                existingGroups[0].add(gridPoint)
+            } else {
+                //merge groups, use the resulting group
+                val mergedGroup = existingGroups.fold(existingGroups[0]) { acc, group ->
+                    if (acc == group) acc else acc.apply {
+                        mergeWith(group)
+                    }
+                }
+                mergedGroup.add(gridPoint)
+            }
+        }
+        val sortedGroups = groups.sortedByDescending { it.size }
+        return sortedGroups[0].size * sortedGroups[1].size * sortedGroups[2].size
+    }
+
+    val test = getInput("day09/test")
+
+    checkResult(part1(test), 15)
+    checkResult(part2(test), 1134)
+
+
+    val input = getInput("day09/input")
+    println(part1(input))
+
+    println(part2(input))
+}
